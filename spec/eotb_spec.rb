c45174d5d74c7b9eec15ca94e631c8af74b8a189
Bartlomiej Kozal <bartlomiejkozal@gmail.com>
2010-06-18 19:27:18 +0200

    Added some new tests

--- spec/eotb_spec.rb
+++ spec/eotb_spec.rb
@@ -7,7 +7,27 @@ describe Eotb do
   end
   
   it "should have register_event method" do
-    @eotb.methods.should include("register_event")
+     @eotb.methods.should include(:register_event)
+   end
+  
+  describe ".register_event" do
+    
+    before :each do
+      @register_event = @eotb.register_event
+    end
+    
+    it "should be an array" do
+      @register_event.should be_a(Array)
+    end
+    
+    it "should have three items" do
+      @register_event.should have(3).items
+    end
+    
+    it "last argument should be a hash" do
+      @register_event[2].should be_a(Hash)
+    end
+    
   end
-
+  
 end
