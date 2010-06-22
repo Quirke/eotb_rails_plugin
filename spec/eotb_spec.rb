d340335a924064034fea2cc3af73df21447b8dba
Bartlomiej Kozal <bartlomiejkozal@gmail.com>
2010-06-22 10:16:12 +0200

    Removed old tests

--- spec/eotb_spec.rb
+++ spec/eotb_spec.rb
@@ -2,31 +2,11 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Eotb do
   
-  before :each do
-    @eotb = Eotb.new
+  context "register_event method" do
+
   end
   
-  it "should have register_event method" do
-     @eotb.methods.should include(:register_event)
-   end
-  
-  describe ".register_event" do
-    
-    before :each do
-      @register_event = @eotb.register_event
-    end
-    
-    it "should be an array" do
-      @register_event.should be_a(Array)
-    end
-    
-    it "should have three items" do
-      @register_event.should have(3).items
-    end
-    
-    it "last argument should be a hash" do
-      @register_event[2].should be_a(Hash)
-    end
+  context "connection with rails app" do
     
   end
   
