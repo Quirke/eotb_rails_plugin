d18747c799d65e9adeea56f1bf0a830bd8cdcd49
Bartlomiej Kozal <bartlomiejkozal@gmail.com>
2010-06-22 10:25:47 +0200

    Added tests description

--- spec/eotb_spec.rb
+++ spec/eotb_spec.rb
@@ -3,10 +3,20 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe Eotb do
   
   context "register_event method" do
-
+    
+    it "should get three arguments (actor, action, subject)"
+    
+    it "subject should be a hash"
+    
+    it "should return json" 
+    
   end
   
-  context "connection with rails app" do
+  context "connection with app" do
+    
+    it "should set connection"
+    
+    it "should send json by POST to app"
     
   end
   
