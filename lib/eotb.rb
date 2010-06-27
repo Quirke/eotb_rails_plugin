1c95fb5cb1ec68d41553a90b01049f22b9090d0f
Bartlomiej Kozal <bartlomiejkozal@gmail.com>
2010-06-27 11:17:26 +0200

    Implemented connection test, added port number to connection method

--- lib/eotb.rb
+++ lib/eotb.rb
@@ -4,15 +4,16 @@ require 'json'
 
 class Eotb
   
-  attr_accessor :api_key, :host
+  attr_accessor :api_key, :host, :port
   
-  def initialize(api_key, host = '127.0.0.1:3000')
+  def initialize(api_key, host = '127.0.0.1', port = '3000')
     @api_key = api_key
     @host = host
+    @port = port
   end
   
   def set_connection
-    http = Net::HTTP.new(host)
+    http = Net::HTTP.new(host, port)
     path = '/apps/' + api_key.to_s + '/events'
   end
   
--- spec/eotb_spec.rb
+++ spec/eotb_spec.rb
@@ -2,7 +2,11 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Eotb do
   
-  it "should check connection status" 
+  it "should check connection status" do
+    eotb = Eotb.new('0')
+    response = Net::HTTP.new(eotb.host, eotb.port).get('/')
+    response.code.should eql('200')
+  end
   
   it "should return data in json" do
     actor = :actor
