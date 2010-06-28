d8a0abbe30e77f81fb3932dcf21a6a50a067b9e1
Bartlomiej Kozal <bartlomiejkozal@gmail.com>
2010-06-28 11:27:23 +0200

    Added http getter

--- lib/eotb.rb
+++ lib/eotb.rb
@@ -4,22 +4,24 @@ require 'json'
 
 class Eotb
   
-  attr_reader :http
-  
   def initialize(api_key)
-    @path = '/apps/' + api_key.to_s + '/events'
+    @@path = '/apps/' + api_key.to_s + '/events'
   end
-  
-  def configure(host = '127.0.0.1', port = '3000')
-    @http = Net::HTTP.new(host, port)
+    
+  def self.configure(host = '127.0.0.1', port = '3000')
+    @@http = Net::HTTP.new(host, port)
   end
   
   def to_json(array)
     JSON.generate(array)
   end
   
-  def register_event(actor, action, subject = nil)
-    @http.post(@path, [actor, action, subject].compact.to_json)
+  def self.http
+    @@http
+  end
+  
+  def self.register_event(actor, action, subject = nil)
+    @@http.post(@@path, [actor, action, subject].compact.to_json)
   end
   
 end
