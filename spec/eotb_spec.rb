cbb1cbcc1e3f3bb4e9a7cc2d302e75101e564dd4
Bartlomiej Kozal <bartlomiejkozal@gmail.com>
2010-07-02 12:13:23 +0200

    Removed unnecessary test

--- spec/eotb_spec.rb
+++ spec/eotb_spec.rb
@@ -2,14 +2,6 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Eotb do
   
-  before(:each) do
-    Eotb.new()
-    Eotb.configure('0', '127.0.0.1', '3000')
-    @data = ['actor', 'action', 'username']
-  end
   
-  it "should post data to unknown server" do
-    Eotb.register_event(@data[0], @data[1], @data[2]).code.should eql('500')
-  end
   
 end
