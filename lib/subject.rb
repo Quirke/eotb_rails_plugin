7b3419687919526dfb755911a658902b0a9e8ed2
Bartlomiej Kozal <bartlomiejkozal@gmail.com>
2010-07-02 13:25:12 +0200

    Implemented Subject class

--- lib/subject.rb
+++ lib/subject.rb
@@ -1,7 +1,22 @@
 class Subject
   
   def initialize(object)
-    object.inspect
+    object.each do |key, value|
+       @subject["event[subject][#{key.to_s}]"] = value.to_s
+    end
+    @subject
+  end
+  
+  def inspect
+    @subject.inspect
+  end
+  
+  def to_hash
+    JSON.parse(@subject)  
+  end
+  
+  def to_json
+    JSON.generate(@subject)    
   end
   
 end
