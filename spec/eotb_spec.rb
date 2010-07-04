f235986d90a4de93ef4203e78b351e31a9df27c7
Bartlomiej Kozal <bartlomiejkozal@gmail.com>
2010-07-04 16:16:54 +0200

    [#4087230] Described new tests

--- spec/eotb_spec.rb
+++ spec/eotb_spec.rb
@@ -2,6 +2,26 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Eotb do
   
+  before(:each) do
+    Eotb.configure("4c3089feb3dfae0304000004")
+  end
   
+  it "should register strings"
+  
+  it "should register symbols"
+  
+  it "should register objects with to_actor and to_subject"
+  
+  it "should not register object without to_actor and to_subject"
+  
+  it "should register hashes"
+  
+  it "should register arrays"
+  
+  it "should generate JSON from event"
+  
+  it "should parse JSON to event (by to_hash method)"
+  
+  it "should inspect event (by inspect method)"
   
 end
