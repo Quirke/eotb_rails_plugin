c97c4910d349349eb6da334bfaca1bc9c149caa2
Bartlomiej Kozal <bartlomiejkozal@gmail.com>
2010-07-05 00:17:13 +0200

    [#4084873] Now only Hash can be a Subject

--- lib/object.rb
+++ lib/object.rb
@@ -1,11 +1,15 @@
 class Object
   
-  def to_subject
-    EotbSubject.new(self).subject
-  end
-  
   def to_actor
     EotbActor.new(self).actor
+  end
+  
+end
+
+class Hash
+  
+  def to_subject
+    EotbSubject.new(self).subject
   end
   
 end
