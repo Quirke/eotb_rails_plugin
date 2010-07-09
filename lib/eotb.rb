07814b6576c0704753f5f26e6fd6e9a56a49983d
Bartlomiej Kozal <bartlomiejkozal@gmail.com>
2010-07-09 12:18:04 +0200

    Added checking respond to method to_hash

--- lib/eotb.rb
+++ lib/eotb.rb
@@ -26,6 +26,8 @@ class Eotb
   def self.format(object)
     if object.respond_to? :to_json
       object.to_json
+    elsif object.respond_to? :to_hash
+        object.to_hash.to_json
     else
       object.inspect
     end
