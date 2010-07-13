8e80332885b81f9d24cca86d7c134e6e94b1b9a2
Bartlomiej Kozal <bartlomiejkozal@gmail.com>
2010-07-13 12:21:34 +0200

    Added new tests

--- spec/eotb_spec.rb
+++ spec/eotb_spec.rb
@@ -2,9 +2,21 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Eotb do
   
-  before(:each) do
+  before(:all) do
     @response = "200"
     Eotb.configure("0"*24)
+  end
+  
+  it "should format actor in string" do
+    Eotb.format("actor").should == {"event[actor]" => "actor"}
+  end
+  
+  it "should format actor in hash" do
+    Eotb.format({:type => "User"}).should == {"event[actor][type]" => "User"}
+  end
+  
+  it "should format actor in nested hash" do
+    Eotb.format({:type => { :account => "User"}}).should == {"event[actor][type][account]" => "User"}
   end
   
   it "should register two arguments" do
@@ -20,15 +32,19 @@ describe Eotb do
   end
   
   it "should register objects" do
-     Eotb.register_event(Object.new, :action, {:username => Object.new}).code.should == @response
+    Eotb.register_event(Object.new, :action, {:username => Object.new}).code.should == @response
   end
 
   it "should register hashes" do
-     Eotb.register_event({:type => "User"}, :action, {:username => {:first_name => "John"}}).code.should == @response
+    Eotb.register_event({:type => "User"}, :action, {:username => {:first_name => "John"}}).code.should == @response
+  end
+  
+  it "should register nested hashes" do
+    Eotb.register_event({:type => { :account => "User"}}, :action, {:username => {:first_name => "John"}}).code.should == @response
   end
   
   it "should register arrays" do
-     Eotb.register_event([2,3,4], :action, {:username => ["John", "Josh"]}).code.should == @response
+    Eotb.register_event([2,3,4], :action, {:username => ["John", "Josh"]}).code.should == @response
   end
   
 end
